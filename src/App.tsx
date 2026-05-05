c3056f05b797925e798d843a98a6973d1125866a
shlmt <sh7618sh@gmail.com>
2026-05-05 18:58:06 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+      <div className="static mb-5 bg-rose-700 p-3 text-lg font-bold text-rose-100">
+        Enventiva
+      </div>
+
+  );
+};
+
+export default App;
